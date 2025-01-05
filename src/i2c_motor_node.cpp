ad9002e0b8026d4a66c3609b6612924f862fe500
Alejandro Gomez <alejodiscovery20@gmail.com>
2025-01-05 13:25:25 -0500

    chore: first commit

---
+++ src/i2c_motor_node.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+#include "rclcpp/rclcpp.hpp"
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("hello world i2c_motor_driver package\n");
+  return 0;
+}
