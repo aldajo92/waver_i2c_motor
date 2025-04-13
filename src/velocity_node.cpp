2d7ac3ff30d95e39319863cbdf656fd7b4bd89d2
Alejandro Gomez <alejodiscovery20@gmail.com>
2025-04-13 00:34:01 -0500

    chore: renamed variables

--- src/velocity_node.cpp
+++ src/velocity_node.cpp
@@ -1,5 +1,6 @@
 #include <rclcpp/rclcpp.hpp>
 #include <geometry_msgs/msg/twist.hpp>
+#include <std_msgs/msg/string.hpp>
 #include <chrono>
 #include <cmath>
 #include <fcntl.h>
@@ -14,18 +15,19 @@ public:
     I2CVelocityNode() : Node("i2c_velocity_node") {
         declare_parameter("i2c_device", "/dev/i2c-1");
         declare_parameter("i2c_address", 0x11);
-        declare_parameter("wheel_radius", 0.08);  // in meters
-        declare_parameter("wheel_base", 0.128);    // in meters
+        declare_parameter("wheel_radius", 0.08); // in meters
+        declare_parameter("wheel_base", 0.128);  // in meters
         declare_parameter("polling_rate_hz", 10.0);
 
         get_parameter("i2c_device", i2c_device_);
         get_parameter("i2c_address", i2c_address_);
-        get_parameter("wheel_radius", R);
-        get_parameter("wheel_base", L);
+        get_parameter("wheel_radius", wheel_radius);
+        get_parameter("wheel_base", wheel_base);
         double freq;
         get_parameter("polling_rate_hz", freq);
 
         vel_pub_ = create_publisher<geometry_msgs::msg::Twist>("/vel_from_i2c", 10);
+        vel_string_pub_ = create_publisher<std_msgs::msg::String>("/vel_from_i2c_string", 10);
 
         file_ = open(i2c_device_.c_str(), O_RDWR);
         if (file_ < 0 || ioctl(file_, I2C_SLAVE, i2c_address_) < 0) {
@@ -40,7 +42,8 @@ public:
     }
 
     ~I2CVelocityNode() {
-        if (file_ >= 0) close(file_);
+        if (file_ >= 0)
+            close(file_);
     }
 
 private:
@@ -57,24 +60,28 @@ private:
         double rpmL = rpm_l / 100.0;
         double rpmR = rpm_r / 100.0;
 
-        double v_l = (rpmL * 2 * M_PI * R) / 60.0;
-        double v_r = (rpmR * 2 * M_PI * R) / 60.0;
+        double v_l = (rpmL * 2 * M_PI * wheel_radius) / 60.0;
+        double v_r = (rpmR * 2 * M_PI * wheel_radius) / 60.0;
 
         double v = (v_r + v_l) / 2.0;
-        double w = (v_r - v_l) / L;
+        double w = (v_r - v_l) / wheel_base;
 
         geometry_msgs::msg::Twist twist;
         twist.linear.x = v;
         twist.angular.z = w;
-
         vel_pub_->publish(twist);
+
+        std_msgs::msg::String msg;
+        msg.data = "v_l: " + std::to_string(v_l) + ", v_r: " + std::to_string(v_r);
+        vel_string_pub_->publish(msg);
     }
 
     std::string i2c_device_;
     int i2c_address_;
-    double R, L;
+    double wheel_radius, wheel_base;
     int file_;
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr vel_pub_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr vel_string_pub_;
     rclcpp::TimerBase::SharedPtr timer_;
 };
 
